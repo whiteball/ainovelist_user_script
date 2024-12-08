cdb06ef09087dd24d3fd4919134585f5bc9df9ba
whiteball <white_ball_11@hotmail.com>
2024-12-09 01:22:31 +0900

    本文の色分けを維持したままクリップボードへコピーできる設定を追加した。

--- scripts/ai_novelist_clipboard_import_export.user.js
+++ scripts/ai_novelist_clipboard_import_export.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AIのべりすと 本文をクリップボードにコピー／クリップボードで本文を上書き
 // @namespace    https://ai-novelist-share.geo.jp/
-// @version      0.1.1
+// @version      0.2.0
 // @description  AIのべりすと編集ページにある環境設定(デスクスタンドのアイコン)の「インポート／エクスポート」の下に、「本文をクリップボードにコピー」ボタンと「クリップボードで本文を完全に上書き」ボタンを追加します。
 // @author       しらたま
 // @match        https://ai-novel.com/novel.php
@@ -14,6 +14,7 @@
 
 /*
 更新履歴
+2024/12/09 0.2.0 色分けを維持したままコピーできる設定を追加。
                  「クリップボードで本文を完全に上書き」でクリップボードにhtmlしかなくてもペーストできるようにした。
                  Firefoxでもクリップボードの取得ができるようになったので、記述を修正。
 2023/03/29 0.1.1 コメントを除外するとき、1つ目のコメント以外が消えていなかったのを修正。
@@ -27,39 +28,77 @@
     }
 
     file_save.insertAdjacentHTML('afterend', `<hr noshade=""><br>
-<input type="button" id="mod_copy_clipboard" value="本文をクリップボードにコピー" class="btn-square" style="color: #358e47; font-size: 16px; background-color:white; border-color: #358e47; width:50vw; margin-right:1vw; font-family: Quicksand;margin-bottom:0.5rem;">
+<input type="button" id="mod_copy_clipboard" value="本文をクリップボードにコピー" class="btn-square" style="color: #358e47; font-size: 16px; background-color:white; border-color: #358e47; width:100%; margin-right:1%; font-family: Quicksand;margin-bottom:0.5rem;">
 <div style="display:none" id="mod_copy_clipboard_dummy"></div><br>
-<label><input type="checkbox" style="font-size: 18px; transform:scale(1.5);" id="mod_copy_clipboard_exclude_comment"><span class="explanations">　コメント(@_や@endpointなど)を除去</span></label>` +
+<label><input type="checkbox" style="font-size: 18px; transform:scale(1.5);" id="mod_copy_clipboard_exclude_comment"><span class="explanations">　コメント(@_や@endpointなど)を除去</span></label><br>
+<label><input type="checkbox" style="font-size: 18px; transform:scale(1.5);" id="mod_copy_clipboard_keep_color"><span class="explanations">　色を維持(htmlとしてコピー)</span></label>` +
 (navigator.clipboard.readText ? `<hr noshade=""><br>
-<input type="button" id="mod_copy_clipboard_paste" value="クリップボードで本文を完全に上書き" class="btn-square" style="color: #d81a1a; font-size: 16px; background-color:white; border-color: #d81a1a; width:50vw; margin-right:1vw; font-family: Quicksand;margin-bottom:0.5rem;">` : ``))
+<input type="button" id="mod_copy_clipboard_paste" value="クリップボードで本文を完全に上書き" class="btn-square" style="color: #d81a1a; font-size: 16px; background-color:white; border-color: #d81a1a; width:100%; margin-right:1%; font-family: Quicksand;margin-bottom:0.5rem;">` : ``))
+    /** @type {HTMLButtonElement|null} */
     const mod_copy_clipboard = document.getElementById('mod_copy_clipboard')
+    /** @type {HTMLDivElement|null} */
     const mod_copy_clipboard_dummy = document.getElementById('mod_copy_clipboard_dummy')
+    /** @type {HTMLInputElement|null} */
     const mod_copy_clipboard_exclude_comment = document.getElementById('mod_copy_clipboard_exclude_comment')
+    /** @type {HTMLInputElement|null} */
+    const mod_copy_clipboard_keep_color = document.getElementById('mod_copy_clipboard_keep_color')
+    /** @type {HTMLButtonElement|null} */
     const mod_copy_clipboard_paste = document.getElementById('mod_copy_clipboard_paste')
-    if (!mod_copy_clipboard || !mod_copy_clipboard_dummy || !mod_copy_clipboard_exclude_comment) {
+    if (!mod_copy_clipboard || !mod_copy_clipboard_dummy || !mod_copy_clipboard_exclude_comment || !mod_copy_clipboard_keep_color) {
         return
     }
 
     mod_copy_clipboard.addEventListener('click', function () {
-        let text = localStorage.textdata.replace(/(<br*?>)/gi, '\n')
-            .replace(/(<div.*?>)/gi, '\n')
-            .replace(/(&nbsp;)/gi, ' ')
-            .replace(/(&amp;)/gi, '&')
-            .replace(/(&lt;)/gi, '<')
-            .replace(/(&gt;)/gi, '>')
-            .replace(/<("[^"]*"|'[^']*'|[^'">])*>/gi, '')
+        /** @type {string} */
+        let text = localStorage.textdata
+        let mime = 'text/plain'
+        if (mod_copy_clipboard_keep_color.checked) {
+            mime = 'text/html'
+            text = text.replace(/(<br[^>]*?><\/div>)/gi, '<\/div>')
 
-        if (mod_copy_clipboard_exclude_comment.checked) {
-            // コメント除去
-            text = text.replace(/^(\w|\W)*[@＠](startpoint|break)(\n|$)/gi, '')
-                .replace(/[@＠]endpoint(?:\n|$)(?:\w|\W)*$/gi, '')
-                .replace(/[@＠][\/／][\*＊]\n(?:\w|\W)*?[@＠][\*＊][\/／](?:\n?)/gi, '')
-                .replace(/^[@＠][_＿][^\n]*(?:\n|$)/gi, '')
+            const vis_fontcolor_ai = document.getElementById("vis_fontcolor_ai")
+            if (vis_fontcolor_ai && vis_fontcolor_ai.value !== 'auto') {
+                text = text.replaceAll('class="textcolor_ai"', 'class="textcolor_ai" style="color: ' + vis_fontcolor_ai.value + '"')
+            } else {
+                colsh = window.Colorscheme_Load( document.getElementById("vis_bgcolor").value )
+                if( !colsh ){
+                    colsh.fcolor_ai = '#2d509e'
+                }
+                text = text.replaceAll('class="textcolor_ai"', 'class="textcolor_ai" style="color: ' + colsh.fcolor_ai + '"')
+            }
+
+            if (mod_copy_clipboard_exclude_comment.checked) {
+                // コメント除去
+                text = text.replace(/^(\w|\W)*[@＠](startpoint|break)(<br[^>]*?>|(?=<\/div>)|<font[^>]*?>|$)/gi, '')
+                    .replace(/[@＠]endpoint(?:<br[^>]*?>|<\/div>|<font[^>]*?>)(?:\w|\W)*$/gi, '')
+                    .replace(/(<font color="#aaaaaa">)?[@＠][\/／][\*＊](<br[^>]*?>|<\/div>|<font[^>]*?>)(?![^@＠]*[@＠][\/／][\*＊](?:<br[^>]*?>|<\/div>|<font[^>]*?>))(?:\w|\W)*?[@＠][\*＊][\/／](?:(<br[^>]*?>|(?=<\/div>)|<\/?font[^>]*?>)?)/gi, '')
+                    .replace(/(<font color="#aaaaaa">)?(?<=>)[@＠][_＿](?!.*<font[^>]*?>).*?(?:<br[^>]*?>|(?=<\/div>)|$)/gi, '')
+                    .replace(/(<font color="#aaaaaa">)?(?<=>)[@＠][_＿].*?<font[^>]*?>.*?(?:<br[^>]*?>|(?=<\/div>)|$)/gi, '')
+                    .replace(/[@＠]([_＿]|break|startpoint|endpoint|\/\*|\*\/)/gi, '')
+            }
+        } else {
+            text = text.replace(/(<br[^>]*?><\/div>)/gi, '\n')
+                .replace(/(<br[^>]*?>)/gi, '\n')
+                .replace(/(<\/div>)/gi, '\n')
+                .replace(/(&nbsp;)/gi, ' ')
+                .replace(/(&amp;)/gi, '&')
+                .replace(/(&lt;)/gi, '<')
+                .replace(/(&gt;)/gi, '>')
+                .replace(/<("[^"]*"|'[^']*'|[^'">])*>/gi, '')
+            if (mod_copy_clipboard_exclude_comment.checked) {
+                // コメント除去
+                text = text.replace(/^(\w|\W)*[@＠](startpoint|break)(\n|$)/gi, '')
+                    .replace(/[@＠]endpoint(?:\n|$)(?:\w|\W)*$/gi, '')
+                    .replace(/[@＠][\/／][\*＊]\n(?![^@＠]*[@＠][\/／][\*＊]\n)(?:\w|\W)*?[@＠][\*＊][\/／](?:\n?)/gi, '')
+                    .replace(/^[@＠][_＿][^\n]*(?:\n|$)/gi, '')
+                    .replace(/[@＠]([_＿]|break|startpoint|endpoint|\/\*|\*\/)/gi, '')
+            }
         }
 
+
         if (window.ClipboardItem && navigator.clipboard.write) {
-            const blob_text = new Blob([text], { type: 'text/plain' }),
-                item = [new window.ClipboardItem({ 'text/plain': blob_text })]
+            const blob_text = new Blob([text], { type: mime }),
+                item = [new window.ClipboardItem({ [mime]: blob_text })]
 
             navigator.clipboard.write(item).then(() => {
                 this.setAttribute('value', '本文をクリップボードにコピーしました！')
